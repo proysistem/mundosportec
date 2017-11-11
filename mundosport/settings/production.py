bccdaa1ef7779be4f912c045833d789c0e40e470
Richard Cotrina <rcotrina@pytel.pe>
2017-11-11 13:26:32 -0500

    Deployment

--- mundosport/settings/production.py
+++ mundosport/settings/production.py
@@ -36,14 +36,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-#STATICFILES_DIRS = (
-#os.path.join(BASE_DIR, 'mundosport', 'static'),
-#)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.child('media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ALLOWED_HOSTS = ['*']
