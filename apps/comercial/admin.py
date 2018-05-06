1e74c75d066a9be289f307a6db11810f4faf99be
proysistem <proysistem@yahoo.com>
2018-05-06 18:44:33 -0400

    control n/c

--- apps/comercial/admin.py
+++ apps/comercial/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
-from .models import Cliente, Proveedor, Vendedor, Movinvent, Pedido, Factura, Compra
+from .models import (Cliente, Proveedor, Vendedor, Movinvent, Pedido, Factura, Compra, Ajustecr,
+                     Notacred)
 
 admin.site.register(Cliente)
 admin.site.register(Proveedor)
 admin.site.register(Vendedor)
 admin.site.register(Compra)
 admin.site.register(Factura)
+admin.site.register(Ajustecr)
+admin.site.register(Notacred)
 admin.site.register(Movinvent)
 
 
